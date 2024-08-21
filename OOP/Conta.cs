1b9a8ffa73f66ec418d7e7c7560a98daf2c29dcb
JOAO COSTA <joao.costa408@etec.sp.gov.br>
2024-08-21 16:08:00 -0300

    Add project files.

---
+++ OOP/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Conta // É necessário esse modificador "public" para que a classe fique visível
+    {
+        public double Saldo { get; set; } // "get" indica que o atributo Saldo pode ser acessado, e o "set" indica que o atributo Saldo pode ter algum valor adicionado
+        public double Limite { get; set; }
+        public int Numero { get; set; }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP;
+
+Console.WriteLine("Aprendendo OOP!");
+
+Conta conta1 = new Conta(); // Criando um objeto ao criar uma instância da class Conta
+
+Conta conta2 = new Conta(); // Criando 2º instância da conta
+
+// Atribuindo valores para a conta 1
+conta1.Saldo = 1500;
+conta1.Limite = 500;
+conta1.Numero = 123;
+
+// Atribuindo valores para a conta 2
+conta2.Saldo = 2500;
+conta2.Limite = 700;
+conta2.Numero = 456;
+
+Console.WriteLine("Olá, Beltrano! Seu saldo em conta é de: " + conta1.Saldo + " reais");
+Console.WriteLine("Limite de saque: " + conta1.Limite + " reais");
+Console.WriteLine("O número de sua conta é: " + conta1.Numero);
+
+Console.WriteLine("________________________________________________________________________");
+
+Console.WriteLine("Olá, Ciclano! Seu saldo em conta é de: " + conta2.Saldo + " reais");
+Console.WriteLine("Limite de saque: " + conta2.Limite + " reais");
+Console.WriteLine("O número de sua conta é: " + conta2.Numero);
