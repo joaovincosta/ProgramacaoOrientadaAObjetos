1e988d4f940ecdc723c161003ae0682d109c3d30
JOAO COSTA <joao.costa408@etec.sp.gov.br>
2024-08-21 16:16:20 -0300

    Agregação

---
+++ Agregacao/Agregacao/Agregacao/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using  Agregacao;
+
+Console.WriteLine("Aprendendo OOP!");
+
+// Criando um objeto de cada classe
+
+CartaoDeCredito cdc = new CartaoDeCredito();
+Cliente c = new Cliente();
+
+// Adicionando nome do cliente
+c.Nome = "João Vini. Costa";
+
+// Adicionando número e data de validade do cartão
+
+cdc.Numero = "122436";
+cdc.DataDeValidade = "03/2027";
+cdc.Cliente = c;
+
+Console.WriteLine("O nome do cliente è: " + c.Nome);
+Console.WriteLine("O número do cartão è: " + cdc.Numero);
+Console.WriteLine("A data de validade do cartão è: " + cdc.DataDeValidade);
+Console.WriteLine("O nome do cliente do agregado è: " + cdc.Cliente.Nome);
+
+
+
+
+
+/* Conta conta = new Conta(123, 300);
+
+// conta.AdicionarLimite(1500);
+
+conta.Deposita(1500);
+conta.Deposita(2500);
+conta.Deposita(4500);
+
+bool saca = conta.Saca(7000);
+
+if (saca)
+{
+    double saldo = conta.ConsultaSaldoDisponivel();
+
+    Console.WriteLine("Seu saldo é de: " + saldo);
+    Console.WriteLine("Seu limite é de: " + conta.Limite);
+    Console.WriteLine("O número da sua conta é: " + conta.Numero);
+} */
